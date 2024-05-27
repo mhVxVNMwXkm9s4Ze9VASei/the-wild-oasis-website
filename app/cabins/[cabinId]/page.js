9aea1037a7f91db5b21a7e2d6794f3c5bad12980
mhVxVNMwXkm9s4Ze9VASei <jediaz95@protonmail.com>
2024-05-27 00:09:43 -0500

    Rendered a client component in a server component.

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,3 +1,4 @@
+import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
@@ -10,7 +11,6 @@ export async function generateMetadata({ params: { cabinId } }) {
 
 export async function generateStaticParams() {
   const cabins = await getCabins();
-
   const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
 
   return ids;
@@ -36,7 +36,9 @@ export default async function Page({ params: { cabinId } }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
